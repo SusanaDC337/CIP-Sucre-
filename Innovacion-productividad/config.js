3492836723b94532bfb723c212c648e8878333a3
SusanaDC337 <susanadconsulting@gmail.com>
2026-09-18 12:11:09 -0500

    Add files via upload

---
+++ Innovacion-productividad/config.js
@@ -0,0 +1,7 @@
+/* URLs de los flujos de Power Automate y Formspree */
+window.CIP_CONFIG = {
+  STATUS_ENDPOINT: "",      // Ej: https://.../status
+  RESERVA_ENDPOINT: "https://defaultded4b5ac325f4214943922d98c01b9.c8.environment.api.powerplatform.com:443/powerautomate/automations/direct/cu/26/workflows/8ea193427aa94202855be11e32bfcf01/triggers/manual/paths/invoke?api-version=1&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=RE_KCSKbp3N_nPO9ormztrESa1zd0b5AP-fN5KgNhQg",
+  FORMSPREE_ENDPOINT: "",   // Ej: https://formspree.io/f/XXXXXXXX
+  DATOS_ENDPOINT: "https://defaultded4b5ac325f4214943922d98c01b9.c8.environment.api.powerplatform.com/powerautomate/automations/direct/cu/29/workflows/80f9db1ffc5d477d8778ad1282823cdf/triggers/manual/paths/invoke?api-version=1&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=GJ9gT6Om1fVRgn8VmYGCqJHgIuaQL5X5tuhgak2M14A"
+};
